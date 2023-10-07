d6ccb2dc335107582f040d1bea87bf2b9dcc4710
Guilherme <gulhermegui772001@gmail.com>
2023-10-06 21:38:49 -0300

    quase

--- src/Components/MusicCard.tsx
+++ src/Components/MusicCard.tsx
@@ -1,29 +1,48 @@
-function MuscicCard(prop) {
-  const { musica } = prop;
+import React, { useState } from 'react';
+
+function MusicCard(props) {
+  const { musica } = props;
+  const [favoritada, setFavoritada] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setFavoritada(!favoritada);
+  };
+
   return (
     <div>
       <ul>
+        <li>{musica.trackId}</li>
+        <li>{musica.trackName}</li>
         <li>
-          {musica.trackId }
-        </li>
-        <li>
-          {musica.trackName }
-        </li>
-        <li>
-          <audio data-testid="audio-component" src="{musica.previewUrl}" controls>
+          <audio data-testid="audio-component" src={ musica.previewUrl } controls>
             <track kind="captions" />
             O seu navegador não suporta o elemento
-            {' '}
             {' '}
             <code>audio</code>
             .
           </audio>
         </li>
+        <li>{musica.kind}</li>
         <li>
-          {musica.kind }
+          <label
+            data-testid={ `checkbox-music-${musica.trackId}` }
+          >
+            <input
+              type="checkbox"
+              checked={ favoritada }
+              onChange={ handleCheckboxChange }
+            />
+            <img
+              src={ favoritada
+                ? '/src/images/checked_heart.png'
+                : '/src/images/empty_heart.png' }
+              alt="favorite"
+            />
+          </label>
         </li>
       </ul>
     </div>
   );
 }
-export default MuscicCard;
+
+export default MusicCard;
